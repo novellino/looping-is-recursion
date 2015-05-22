329c2cdcecb24d91db02cd940669b848e1f0082f
Marc <marc.novel@gmail.com>
2015-05-22 15:26:33 +0200

    Second Attempt

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,86 +1,70 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  (loop [helper (fn [acc n]
+  (let [helper (fn [acc n]
                  (cond
-                  (zero? n) 1
-                  (= n 1) acc
-                  :else (recur (* acc base) (dec n))))]
+                   (= n 0) 1
+                   (= n 1) acc
+                   :else (recur (* acc base) (dec n))))]
     (helper base exp)))
 
+
 (defn last-element [a-seq]
-  (loop [helper (fn [acc n]
+  (let [helper (fn [acc n]
                  (cond
-                  (= n 0) nil
-                  (= n 1) (first acc)
-                  :else (recur (rest acc) (dec n))))]
+                   (= n 0) nil
+                   (= n 1) (first acc)
+                 :else (recur (rest acc) (dec n))))]
     (helper a-seq (count a-seq))))
 
-
 (defn seq= [seq1 seq2]
-  (let [helper (fn [acc1 acc2 n1 n2]
+  (let [helper (fn [acc1 acc2 n]
                  (cond
-                  (and (= n1 0) (= n2 0)) true
-                  (= (first acc1) (first acc2))
-                    (recur (rest acc1) (rest acc2) (dec n1) (dec n2))
-                  :else false))]
-    (helper seq1 seq2 (count seq1) (count seq2))))
-
-
-;; ;; (defn seq= [seq1 seq2])
-
-
-
-;; ;; (defn find-first-index [pred a-seq]
-;; ;;   (loop [acc 0
-;; ;;          my-seq a-seq]
-;; ;;     (if (pred (first my-seq))
-;; ;;       acc
-;; ;;       (recur (rest my-seq) (inc acc)))))
-
+                   (= n 0) true
+                   (= (first acc1) (first acc2)) (recur (rest acc1) (rest acc2) (dec n))
+                   :else false ))]
+    (cond
+      (= (count seq1) (count seq2)) (helper seq1 seq2 (count seq1))
+      :else false)))
 
 (defn find-first-index [pred a-seq]
-  (loop [my-seq a-seq
-         acc 0]
+  (loop [acc a-seq
+         n 0]
     (cond
-     (empty? my-seq) nil
-     (pred (first my-seq)) acc
-     :else (recur (rest my-seq) (inc acc)))))
+      (= n (count a-seq)) nil
+      (pred (first acc)) n
+      :else (recur (rest acc) (inc n)))))
 
-
-(defn avg-rec [a-seq]
-  (let [quot (count a-seq)]
+(defn avg [a-seq]
   (loop [acc 0
+         n 0
          my-seq a-seq]
-         (if (empty? my-seq) (/ acc quot)
-           (recur (+ acc (first my-seq)) (rest my-seq))))))
-
-
+    (cond
+      (empty? my-seq) (/ acc n)
+      :else (recur (+ acc (first my-seq)) (inc n) (rest my-seq)))))
 
 (defn parity [a-seq]
-  (loop [my-seq a-seq
-         acc #{}]
-    (let [elem (first my-seq)]
-      (cond
-       (empty? my-seq) acc
-       (contains? acc elem) (recur (rest my-seq) (disj acc elem))
-       :else (recur (rest my-seq) (conj acc elem))))))
+  (loop [acc #{}
+         my-seq a-seq]
+    (cond
+      (empty? my-seq) acc
+      (contains? acc (first my-seq)) (recur (disj acc (first my-seq)) (rest my-seq))
+      :else (recur (conj acc (first my-seq)) (rest my-seq)))))
 
-
- (defn fast-fibo [n]
+(defn fast-fibo [n]
    (loop [current 0
           next 1
           n n]
-    (if (zero? n)
-      current
-      (recur next (+ current next) (dec n)))))
+     (cond
+       (zero? n) current
+       :else (recur next (+ current next) (dec n)))))
 
 (defn cut-at-repetition [a-seq]
-  (loop [my-seq a-seq
-         acc #{}]
+ (loop [my-seq a-seq
+        acc #{}
+        result []]
     (let [elem (first my-seq)]
       (cond
-       (empty? my-seq) (vec acc)
-       (contains? acc elem) (vec acc)
-       :else (recur (rest my-seq) (conj acc elem))))))
-
+       (empty? my-seq) result
+       (contains? acc elem) result
+       :else (recur (rest my-seq) (conj acc elem) (conj result elem))))))
